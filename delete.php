f8581bc5773e84d5e2c063c1965325c804f19c53
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-09-09 22:35:53 +0700

    update delete.php

--- delete.php
+++ delete.php
@@ -1,3 +1,12 @@
 <?php
  $db_connection = mysqli_connect("127.0.0.1", "root", "", "profil_teman") or die("Koneksi Gagal");
 ?>
+
+<?php
+if($_GET){
+    $id = $_GET["id"];
+    $sql = "DELETE FROM profil WHERE id ='".$id."'";
+    $result = mysqli_query($db_connection,$sql);   
+    
+    echo '<h3 style="color:black;text-align:center;">Data Berhasil Dihapus</h3>';
+}
